9dc6d139e1bf1a050b3ab51b36ec491717d4262b
Akhramovich_D <dmitry.hrom@gmail.com>
2020-12-12 19:21:43 +0300

    Задание 1

---
+++ 1/script.js
@@ -0,0 +1,48 @@
+let userName = checkStr("Ваше имя?", "Вы не ввели или ввели некорректное имя");
+
+let userMiddleName = checkStr("Ваше отчество?", "Вы не ввели или ввели некорректное отчество");
+
+let userLastName = checkStr("Ваша фамилия?", "Вы не ввели или ввели некорректную фамилию");
+
+let userAge = checkAge("Ваш возраст?");
+
+let sex = confirm("Если Ваш пол мужской нажмите 'Ок', иначе нажмите 'Отмена'")? "мужской" : "женский";
+
+alert(
+    `ваше ФИО: ${userLastName} ${userName} ${userMiddleName} 
+ваш возраст в годах: ${userAge}
+ваш возраст в днях: ${userAge * 365}
+через 5 лет вам будет: ${userAge + 5}
+ваш пол: ${sex}
+вы на пенсии: ${checkPension(userAge, sex)}`
+);
+
+function checkStr(question, wrong) {
+    let str = prompt(question);
+    while(str == null || str === '' || /[0-9]/.test(str)){
+        alert(wrong);
+        str = prompt(question);
+    }
+    return str;
+}
+
+function checkAge(question) {
+    let age = Number(prompt("Ваш возраст?"));
+    while(isNaN(age) || Number(age) > 150 || Number(age) <= 0){
+        alert("Вы не ввели или ввели некорректый возраст");
+        age = Number(prompt(question));
+    }
+    return age;
+}
+
+function checkPension(userAge, sex) {
+    if(userAge >= 63 && sex === 'мужской'){
+        return 'да';
+    }else if (userAge < 63 && sex === 'мужской'){
+        return 'нет';
+    }else if (userAge >= 58 && sex === 'женский'){
+        return 'да';
+    }else {
+        return 'нет';
+    }
+}
