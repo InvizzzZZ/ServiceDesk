ad2f41ca316ff223c1a84251d7f9be76944d732e
Akhramovich_D <dmitry.hrom@gmail.com>
2020-12-30 17:43:28 +0300

    N.03 & N.05

---
+++ N.03/script.js
@@ -0,0 +1,40 @@
+let userName = checkStr("Ваше имя?", "Вы не ввели или ввели некорректное имя");
+
+let userMiddleName = checkStr("Ваше отчество?", "Вы не ввели или ввели некорректное отчество");
+
+let userLastName = checkStr("Ваша фамилия?", "Вы не ввели или ввели некорректную фамилию");
+
+let userAge = checkAge("Ваш возраст?");
+
+let sex = confirm("Если Ваш пол мужской нажмите 'Ок', иначе нажмите 'Отмена'");
+
+alert(
+    `ваше ФИО: ${userLastName} ${userName} ${userMiddleName} 
+ваш возраст в годах: ${userAge}
+ваш возраст в днях: ${userAge * 365}
+через 5 лет вам будет: ${userAge + 5}
+ваш пол: ${sex?'мужской':'женский'}
+вы на пенсии: ${checkPension(userAge, sex)?'да':'нет'}`
+);
+
+function checkStr(question, wrong) {
+    let str = prompt(question);
+    while(str == null || str === '' || /[0-9]/.test(str)){
+        alert(wrong);
+        str = prompt(question);
+    }
+    return str;
+}
+
+function checkAge(question) {
+    let age = Number(prompt("Ваш возраст?"));
+    while(isNaN(age) || Number(age) > 150 || Number(age) <= 0){
+        alert("Вы не ввели или ввели некорректый возраст");
+        age = Number(prompt(question));
+    }
+    return age;
+}
+
+function checkPension(userAge, sex) {
+    return ((userAge >= 63 && sex) ? true : ((userAge < 63 && sex) ? false : (userAge >= 58 && sex)));
+}
---
+++ N.05/common.js
@@ -0,0 +1,22 @@
+let arr = [5, 7,
+          [4, [2], 8, [1, 3], 2],
+          [9, []],
+          1, 8
+          ];
+
+alert(treeSum(arr));
+
+function treeSum(arrParam) {
+    let sum =  0;
+    arrParam.forEach(function (item, i, arr) {
+        if (typeof item !== 'object') {
+            sum += item;
+        } else {
+            sum += treeSum(arr[i]);
+        }
+    });
+    return sum;
+}
+
+
+
